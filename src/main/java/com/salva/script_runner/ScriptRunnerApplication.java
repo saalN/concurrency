aaed7b9442c8be45ed7bf9b5a1f17c5f26074732
saalN <ususale22@gmail.com>
2025-05-08 17:16:42 +0200

    first commit

---
+++ src/main/java/com/salva/script_runner/ScriptRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.salva.script_runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salva/script_runner/ScriptRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salva.script_runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
